53898f220677d8ff3b40ff6b22c43c1f83998d8f
Lee Zi Jie <103517478+zjiee@users.noreply.github.com>
2026-04-09 15:36:06 +0800

    Add files via upload

---
+++ src/strategy/SpeedBasedTurnOrder.java
@@ -0,0 +1,15 @@
+package strategy;
+
+import model.Combatant;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SpeedBasedTurnOrder implements ITurnOrderStrategy {
+
+    @Override
+    public List<Combatant> determineTurnOrder(List<Combatant> combatants) {
+
+    }
+}
