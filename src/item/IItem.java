005ccd2d9a1f99fbb153e115f2e68afbe6d78ed0
justinlim04 <justlim04@gmail.com>
2026-04-09 15:48:00 +0800

    Update IItem.java

--- src/item/IItem.java
+++ src/item/IItem.java
@@ -1,4 +1,13 @@
 package item;
 
+import model.Combatant;
+import model.Player;
+import engine.BattleContext;
+
+import java.util.List;
+
 public interface IItem {
+    void use(Player player, List<? extends Combatant> enemies, Combatant target, BattleContext ctx);
+    String getName();
+    String getDescription();
 }
