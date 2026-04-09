5c8f6dd99d17bad5a1128bc158e606a4c3e36008
Lee Zi Jie <103517478+zjiee@users.noreply.github.com>
2026-04-09 15:25:43 +0800

    Create SpeedBasedTurnOrder.java

---
+++ SpeedBasedTurnOrder.java
@@ -0,0 +1,17 @@
+package strategy;
+
+import model.Combatant;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SpeedBasedTurnOrder implements ITurnOrderStrategy {
+
+    @Override
+    public List<Combatant> determineTurnOrder(List<Combatant> combatants) {
+        List<Combatant> ordered = new ArrayList<>(combatants);
+        ordered.sort(Comparator.comparingInt(Combatant::getSpeed).reversed());
+        return ordered;
+    }
+}
