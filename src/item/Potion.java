fff01cd3a0f4be7e3f35c543442909cc992c0fc4
justinlim04 <justlim04@gmail.com>
2026-04-10 10:50:51 +0800

    Update Potion.java

--- src/item/Potion.java
+++ src/item/Potion.java
@@ -1,1 +1,31 @@
+package item;
 
+import model.Combatant;
+import model.Player;
+import engine.BattleContext;
+
+import java.util.List;
+
+public class Potion implements IItem {
+
+    private static final int HEAL_AMOUNT = 100;
+
+    @Override
+    public void use(Player player, List<? extends Combatant> enemies, Combatant target, BattleContext ctx) {
+        int before = player.getCurrentHp();
+        player.heal(HEAL_AMOUNT);
+        int after = player.getCurrentHp();
+        ctx.log(player.getName() + " -> Potion used: HP: " + before + " -> " + after
+                + " (+" + (after - before) + ")");
+    }
+
+    @Override
+    public String getName() {
+        return "Potion";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Heal 100 HP (capped at max HP).";
+    }
+}
