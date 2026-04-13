1702f833b5fa12a4b9c384eeac571b5020e383ae
Braedon Liim <braedonlim04@gmail.com>
2026-04-13 19:38:52 +0800

    did remaining changes for special skill action

--- src/effect/StunEffect.java
+++ src/effect/StunEffect.java
@@ -1,5 +1,39 @@
 package effect;
 
-public class StunEffect {
+import model.Combatant;
 
+public class StunEffect implements IStatusEffect {
+
+    private int turnsRemaining;
+
+    public StunEffect(int turns) {
+        this.turnsRemaining = turns;
+    }
+
+    @Override
+    public void onApply(Combatant c) {
+    }
+
+    @Override
+    public void tick() {
+        if (turnsRemaining > 0) turnsRemaining--;
+    }
+
+    @Override
+    public void onExpire(Combatant c) {
+    }
+
+    @Override
+    public boolean isExpired() {
+        return turnsRemaining <= 0;
+    }
+
+    @Override
+    public String getName() {
+        return "Stun";
+    }
+
+    public int getTurnsRemaining() {
+        return turnsRemaining;
+    }
 }
